28e5d3a74e3834aee61f9bf233d45ee2bc736a25
Artem Saikin <art.saikin@gmail.com>
2020-06-20 00:51:02 +0400

    GameBoard preferences

--- GameBoard.cs
+++ GameBoard.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class GameBoard : MonoBehaviour
 {
+    private readonly int _defaultSizeX = 10;
+    private readonly int _defaultSizeY = 10;
+
+    public GameBoard()
+    {
+        _size = new Vector2Int(_defaultSizeX, _defaultSizeY);
+    }
+
     [SerializeField]
     private Transform _cell;
 
@@ -11,6 +19,15 @@ public class GameBoard : MonoBehaviour
     {
         get { return _cell; }
         set { _cell = value; }
+    }
+
+    [SerializeField]
+    private Vector2Int _cellSize;
+
+    public Vector2Int CellSize
+    {
+        get { return _cellSize; }
+        set { _cellSize = value; }
     }
 
     [SerializeField]
@@ -22,15 +39,25 @@ public class GameBoard : MonoBehaviour
         set { _size = value; }
     }
 
+    private void Awake()
+    {
+        InitializeBoard();
+    }
+
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
         
+    }
+
+    private void InitializeBoard()
+    {
+        Cell.localScale = new Vector3(CellSize.x, CellSize.y, 1f);
     }
 }
